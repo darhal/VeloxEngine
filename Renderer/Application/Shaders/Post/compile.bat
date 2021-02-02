4fc7aadaa906b5436d86bc0583dabe0750485ed5
Omarito <chidaomar46@gmail.com>
2021-02-02 23:36:10 +0100

    Problem with optimised version

--- Renderer/Application/Shaders/Post/compile.bat
+++
@@ -1,3 +0,0 @@
-%VULKAN_SDK%\Bin\glslc.exe --target-spv=spv1.4 --target-env=vulkan1.2 post.vert -o post.vert.spv
-%VULKAN_SDK%\Bin\glslc.exe --target-spv=spv1.4 --target-env=vulkan1.2 post.frag -o post.frag.spv
-pause
--- Renderer/Application/Shaders/compile.bat
+++
@@ -1,3 +0,0 @@
-%VULKAN_SDK%\Bin\glslc.exe --target-spv=spv1.4 --target-env=vulkan1.2 shader.vert -o vert.spv
-%VULKAN_SDK%\Bin\glslc.exe --target-spv=spv1.4 --target-env=vulkan1.2 shader.frag -o frag.spv
-pause
