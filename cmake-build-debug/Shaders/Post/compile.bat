52adc180f32d2ba023af8590102248eaa37575f4
Omarito <chidaomar46@gmail.com>
2021-02-07 02:42:10 +0100

    Fixed swapchain resizing on Linux

---
+++ cmake-build-debug/Shaders/Post/compile.bat
@@ -0,0 +1,3 @@
+%VULKAN_SDK%\Bin\glslc.exe --target-spv=spv1.4 --target-env=vulkan1.2 post.vert -o post.vert.spv
+%VULKAN_SDK%\Bin\glslc.exe --target-spv=spv1.4 --target-env=vulkan1.2 post.frag -o post.frag.spv
+pause
